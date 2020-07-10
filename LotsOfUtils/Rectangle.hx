83052767bafd1a85907beafe1d78591c2a6179aa
David Irimia <36113284+daverave1212@users.noreply.github.com>
2020-07-10 15:16:01 +0300

    Uploaded updated LotsOfUtils

--- LotsOfUtils/Rectangle.hx
+++ LotsOfUtils/Rectangle.hx
@@ -17,5 +17,13 @@ class Rectangle{
 	public inline function toString(){
 		return (x + ", " + y + "  " + width + "x" + height);
 	}
+
+	public inline function containsPoint(point : Point){
+		return containsCoordinates(point.x, point.y);
+	}
+
+	public inline function containsCoordinates(_x : Float, _y : Float){
+		return (x <= _x) && (_x <= x + width) && (y <= _y) && (_y <= y + height);
+	}
 	
 }
